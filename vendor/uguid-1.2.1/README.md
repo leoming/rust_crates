3e99e8d0c27d055e37b0fba0d57771e072410abf
Nicholas Bishop <nicholasbishop@google.com>
2022-09-29 11:26:38 -0400

    Run vendor.py after adding crdyboot
    BUG=b:202164480
    TEST=./vendor.py
    
    Change-Id: I02b1c551c719cc78a0daa80bb6573083b10ae97a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/rust_crates/+/3933385
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>
    Commit-Queue: Nicholas Bishop <nicholasbishop@google.com>
    Reviewed-by: George Burgess <gbiv@chromium.org>

---
+++ vendor/uguid-1.2.1/README.md
@@ -0,0 +1,36 @@
+# `uguid`
+
+[![Crates.io](https://img.shields.io/crates/v/uguid)](https://crates.io/crates/uguid) 
+[![Docs.rs](https://docs.rs/uguid/badge.svg)](https://docs.rs/uguid)
+
+`no_std` library providing a GUID (Globally Unique Identifer) type, as
+used in GPT disks, UEFI, and Microsoft Windows.
+
+[GPT]: https://en.wikipedia.org/wiki/GUID_Partition_Table
+
+## Features
+
+No features are enabled by default.
+
+* `bytemuck`: Implements bytemuck's `Pod` and `Zeroable` traits for `Guid`.
+* `serde`: Implements serde's `Serialize` and `Deserialize` traits for `Guid`.
+* `std`: Provides `std::error::Error` implementation for the error type.
+
+## Minimum Supported Rust Version (MSRV)
+
+The current MSRV is 1.60.0 due to the use of the Cargo [`dep:`]
+feature. Feel free to file an issue or create a PR if you have a use
+case that requires an older version.
+
+[`dep:`]: https://blog.rust-lang.org/2022/04/07/Rust-1.60.0.html#new-syntax-for-cargo-features
+
+## License
+
+Licensed under either of [Apache License, Version 2.0](LICENSE-APACHE)
+or [MIT license](LICENSE-MIT) at your option.
+
+## Disclaimer
+
+This project is not an official Google project. It is not supported by
+Google and Google specifically disclaims all warranties as to its quality,
+merchantability, or fitness for a particular purpose.
---
+++ vendor/uguid-1.2.1/tests/test_serde.rs
@@ -0,0 +1,50 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![cfg(feature = "serde")]
+
+use serde_test::Token;
+use uguid::{aligned_guid, guid, AlignedGuid, Guid};
+
+#[test]
+fn test_serde_unaligned() {
+    let guid = guid!("01234567-89ab-cdef-0123-456789abcdef");
+
+    serde_test::assert_tokens(
+        &guid,
+        &[Token::Str("01234567-89ab-cdef-0123-456789abcdef")],
+    );
+
+    serde_test::assert_de_tokens_error::<Guid>(
+        &[Token::Str("1234")],
+        "GUID string has wrong length (expected 36 bytes)",
+    );
+
+    serde_test::assert_de_tokens_error::<Guid>(
+        &[Token::U64(1234)],
+        "invalid type: integer `1234`, expected a string in the format \"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx\"");
+}
+
+#[test]
+fn test_serde_aligned() {
+    let guid = aligned_guid!("01234567-89ab-cdef-0123-456789abcdef");
+
+    serde_test::assert_tokens(
+        &guid,
+        &[Token::Str("01234567-89ab-cdef-0123-456789abcdef")],
+    );
+
+    serde_test::assert_de_tokens_error::<AlignedGuid>(
+        &[Token::Str("1234")],
+        "GUID string has wrong length (expected 36 bytes)",
+    );
+
+    serde_test::assert_de_tokens_error::<AlignedGuid>(
+        &[Token::U64(1234)],
+        "invalid type: integer `1234`, expected a string in the format \"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx\"");
+}
