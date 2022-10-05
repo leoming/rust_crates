aefc71c3152964002b1cbe43b1939211d8af3181
George Burgess IV <gbiv@google.com>
2022-10-05 16:32:27 -0700

    empty_crates: add entries for ~all empty crates in dev-rust/
    I've been in the following situation several times now.
    
    "identify_migration_candidates is identifying nothing."
    "oh, crate $foo is a leaf. let's look at that."
    "oh, crate $foo is an empty crate, well, let's look at its users."
    "oh, its user only depends on this empty crate. i can migrate that
    without any problems by hand."
    
    Adding these empty crates allow the replacement of all empty
    crates with third-party-crates-src.
    
    It's not expected for these to stick around; this is simply to ease
    migration.
    
    BUG=b:240953811
    TEST=./vendor.py
    
    Change-Id: I67df493c229ccd8e89e2cb4635c03a72af4f4ccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/rust_crates/+/3964974
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Tested-by: George Burgess <gbiv@chromium.org>

---
+++ patches/ascii/make_files_non_executable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -eux
+# Copyright 2022 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# This crate shis executable md files. Remove the x bit.
+chmod -x *.md
